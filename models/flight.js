fc5fcea9299298d15e8e84c9c9b9a8960b22ffdd
tbreck09 <royals_15@hotmail.com>
2023-07-14 17:06:58 -0400

    new update

---
+++ models/flight.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const flightSchema = new Schema({
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United']
+    },
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+    },
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999
+    },
+    departs: {
+        type: Date,
+
+    }
+    })
+
+      
+    
+    
+    
+
+module.exports = mongoose.model('Flight', flightSchema)
